e2750fae469191dc088c9c1f95f994769abc6240
Christian <cvsejersen@gmail.com>
2024-06-12 16:19:14 +0200

    Stop tracking build dir

--- build/plugins/bezier_creator/CMakeFiles/bezier_creator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bezier_creator.
-# This may be replaced when dependencies are built.
--- build/plugins/square_animation/CMakeFiles/square_animation.dir/__/__/utils/step_functions.cpp.o.d
+++
@@ -1,5 +0,0 @@
-plugins/square_animation/CMakeFiles/square_animation.dir/__/__/utils/step_functions.cpp.o: \
-  /Users/sejersen/dev/prism/utils/step_functions.cpp \
-  /Users/sejersen/dev/prism/utils/step_functions.h \
-  /opt/homebrew/Cellar/raylib/5.0/include/raylib.h \
-  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdarg.h
